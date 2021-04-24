1052ed1922e8b7ebdcf8e96eb2cb5aa034008bc6
BenTitayassine <bentitayassin@live.fr>
2021-04-24 12:48:53 +0100

    crate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbavComponent } from './navbav/navbav.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbavComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
